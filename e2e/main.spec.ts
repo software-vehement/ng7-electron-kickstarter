64280e07cd32262606017f1c6df855f73abb6f91
Tejas Shailesh Sabunkar <M1048215@mindtree.com>
2019-06-12 13:06:17 +0530

    first commit

---
+++ e2e/main.spec.ts
@@ -0,0 +1,15 @@
+import { SpectronClient } from 'spectron';
+
+import commonSetup from './common-setup';
+
+describe('angular-electron App', function () {
+  commonSetup.apply(this);
+
+  let browser: any;
+  let client: SpectronClient;
+
+  beforeEach(function () {
+    client = this.app.client;
+    browser = client as any;
+  });
+});
